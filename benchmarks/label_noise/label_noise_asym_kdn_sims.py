ace00d1d2dcd5f27035a781a68b08733d4b56461
jdesman1 <jdesman1@jhu.edu>
2022-03-12 15:49:23 -0500

    Update noise simulations, update visualization tool, add plot

--- benchmarks/label_noise/label_noise_asym_kdn_sims.py
+++ benchmarks/label_noise/label_noise_asym_kdn_sims.py
@@ -10,23 +10,40 @@ from kdg.utils import (
     generate_sinewave,
     generate_polynomial,
 )
-from kdg import kdf
-from sklearn.ensemble import RandomForestClassifier as rf
+from kdg import kdf, kdn
+from tensorflow import keras
+from tensorflow.keras import layers
 import pickle
 
+# network architecture
+def getNN():
+    compile_kwargs = {
+        "loss": "binary_crossentropy",
+        "optimizer": keras.optimizers.Adam(3e-4),
+    }
+    network_base = keras.Sequential()
+    network_base.add(layers.Dense(5, activation="relu", input_shape=(2,)))
+    network_base.add(layers.Dense(5, activation="relu"))
+    network_base.add(layers.Dense(units=2, activation="softmax"))
+    network_base.compile(**compile_kwargs)
+    return network_base
 
-def label_noise_trial(func, n_samples, p0=0.0, p1=0.0, n_estimators=500):
+
+def label_noise_trial(func, n_samples, p0=0.0, p1=0.0):
     """Single label noise trial with proportion p0 and p1 of flipped
     labels for class 0 and class 1, respectively.
     Also allows for different data generation functions."""
     if func == generate_gaussian_parity:
         X, y = func(n_samples, cluster_std=0.5)
+        X_val, y_val = func(1000, cluster_std=0.5)
         X_test, y_test = generate_gaussian_parity(1000, cluster_std=0.5)
     elif func == generate_polynomial:
         X, y = func(n_samples, a=[1, 3])
+        X_val, y_val = func(1000, a=[1, 3])
         X_test, y_test = func(1000, a=[1, 3])
     else:
         X, y = func(n_samples)
+        X_val, y_val = func(1000)
         X_test, y_test = func(1000)
 
     # Generate noise and flip labels
@@ -39,14 +56,27 @@ def label_noise_trial(func, n_samples, p0=0.0, p1=0.0, n_estimators=500):
     y[noise_indices0] = 1 - y[noise_indices0]
     y[noise_indices1] = 1 - y[noise_indices1]
 
-    model_kdf = kdf(kwargs={"n_estimators": n_estimators})
-    model_kdf.fit(X, y)
-    error_kdf = 1 - np.mean(model_kdf.predict(X_test) == y_test)
+    # NN params
+    callback = keras.callbacks.EarlyStopping(
+        monitor="val_loss", patience=10, verbose=False
+    )
+    fit_kwargs = {
+        "epochs": 200,
+        "batch_size": 64,
+        "verbose": False,
+        "validation_data": (X_val, keras.utils.to_categorical(y_val)),
+        "callbacks": [callback],
+    }
 
-    model_rf = rf(n_estimators=n_estimators)
-    model_rf.fit(X, y)
-    error_rf = 1 - np.mean(model_rf.predict(X_test) == y_test)
-    return error_kdf, error_rf
+    # Train vanilla NN
+    nn = getNN()
+    nn.fit(X, keras.utils.to_categorical(y), **fit_kwargs)
+    error_nn = 1 - np.mean(np.argmax(nn.predict(X_test), axis=1) == y_test)
+
+    model_kdn = kdn(nn)
+    model_kdn.fit(X, y)
+    error_kdn = 1 - np.mean(model_kdn.predict(X_test) == y_test)
+    return error_kdn, error_nn
 
 
 # Data generation functions
@@ -60,16 +90,14 @@ funcs = [
 func_names = ["GXOR", "Ellipse", "Spirals", "Sinewave", "Polynomial"]
 
 for i in range(len(funcs)):
-    ### Run the experiment with varying proportion of label noise
     df = pd.DataFrame()
     reps = 10
-    n_estimators = 100
     n_samples = 1000
     p0s = np.arange(0, 0.5, 0.05)
     p1s = np.arange(0, 0.5, 0.05)
 
-    err_kdf = []
-    err_rf = []
+    err_kdn = []
+    err_nn = []
     p0_list = []
     p1_list = []
     reps_list = []
@@ -78,28 +106,24 @@ for i in range(len(funcs)):
         for p1 in p1s:
             print("Doing p0 = {:.2f}, p1 = {:.2f}".format(p0, p1))
             for ii in range(reps):
-                err_kdf_i, err_rf_i = label_noise_trial(
-                    func=funcs[i],
-                    n_samples=n_samples,
-                    p0=p0,
-                    p1=p1,
-                    n_estimators=n_estimators,
+                err_kdn_i, err_nn_i = label_noise_trial(
+                    func=funcs[i], n_samples=n_samples, p0=p0, p1=p1
                 )
-                err_kdf.append(err_kdf_i)
-                err_rf.append(err_rf_i)
+                err_kdn.append(err_kdn_i)
+                err_nn.append(err_nn_i)
                 reps_list.append(ii)
                 p0_list.append(p0)
                 p1_list.append(p1)
                 print(
-                    "KDF error = {:.4f}, RF error = {:.4f}".format(err_kdf_i, err_rf_i)
+                    "KDN error = {:.4f}, DN error = {:.4f}".format(err_kdn_i, err_nn_i)
                 )
 
     # Construct DataFrame
     df["reps"] = reps_list
     df["p0"] = p0_list
     df["p1"] = p1_list
-    df["error_kdf"] = err_kdf
-    df["error_rf"] = err_rf
+    df["error_kdn"] = err_kdn
+    df["error_nn"] = err_nn
 
     # Heatmap params
     mapping = np.zeros((len(p0s), len(p1s)))
@@ -107,9 +131,9 @@ for i in range(len(funcs)):
     for j, p0 in enumerate(p0s):
         for k, p1 in enumerate(p1s):
             df_p0_p1 = df[(df["p0"] == p0) & (df["p1"] == p1)]
-            mapping[j, k] = np.median(df_p0_p1["error_kdf"] - df_p0_p1["error_rf"])
+            mapping[j, k] = np.median(df_p0_p1["error_kdn"] - df_p0_p1["error_nn"])
 
     # Save data
-    fname = "data/" + func_names[i] + "_mapping_kdf.pkl"
+    fname = "data/" + func_names[i] + "_mapping_kdn.pkl"
     with open(fname, "wb") as f:
         pickle.dump(mapping, f)
--- benchmarks/label_noise/label_noise_figs.py
+++ benchmarks/label_noise/label_noise_figs.py
@@ -2,75 +2,82 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
-
-# Create KDF figure
-with open("data/xor_heatmap_kdf.pkl", "rb") as f:
-    mapping = pickle.load(f)
-
-# Setup some plotting parameters
-sns.set_context("talk")
-fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-p0s = np.arange(0, 0.5, 0.05)
-p1s = np.arange(0, 0.5, 0.05)
-p0_labels = ["{:.2f}".format(p0) for p0 in p0s]
-p1_labels = ["{:.2f}".format(p1) for p1 in p1s]
-
-# Make every other label invisible
-for j, p1 in enumerate(p1s):
-    if j % 2 == 0:
-        p1_labels[j] = ""
-        p0_labels[j] = ""
-
-sns.heatmap(
-    mapping,
-    xticklabels=p1_labels,
-    yticklabels=p0_labels,
-    cmap="RdBu_r",
-    ax=ax,
-    cbar=True,
-    cbar_kws={"shrink": 0.7},
-    vmin=-0.1,
-    vmax=0.1,
+import pandas as pd
+from kdg.utils import plot_2dsim
+from kdg.utils import (
+    generate_gaussian_parity,
+    generate_ellipse,
+    generate_spirals,
+    generate_sinewave,
+    generate_polynomial,
 )
-ax.set_xlabel("p1")
-ax.set_ylabel("p0")
-ax.set_aspect("equal", "box")
-plt.title("Gaussian XOR Label Noise: KDF - RF Errors")
+
+# 2D simulation plotting
+n_samples = 1e4
+X, y = {}, {}
+X["gxor"], y["gxor"] = generate_gaussian_parity(n_samples)
+X["spiral"], y["spiral"] = generate_spirals(n_samples)
+X["circle"], y["circle"] = generate_ellipse(n_samples)
+X["sine"], y["sine"] = generate_sinewave(n_samples)
+X["poly"], y["poly"] = generate_polynomial(n_samples, a=[1, 3])
+
+# Heatmap plotting
+func_names = ["GXOR", "Spirals", "Ellipse", "Sinewave", "Polynomial"]
+func_labels = ["Gaussian XOR", "Ellipse", "Spiral", "Sinewave", "Polynomial"]
+
+fig, ax = plt.subplots(3, 5, figsize=(12, 6))
+
+for i, key in enumerate(X.keys()):
+    plot_2dsim(X[key], y[key], ax=ax[0, i])
+    ax[0, i].set_aspect("equal")
+    ax[0, i].get_xaxis().set_ticks([])
+    ax[0, i].get_yaxis().set_ticks([])
+    ax[0, i].set_title(func_labels[i])
+
+    if i == 0:
+        ax[0, i].set_ylabel("Simulation Data")
+
+
+# KDF plotting
+for i, f in enumerate(func_names):
+    fname = "data/" + f + "_mapping_kdf.pkl"
+    with open(fname, "rb") as fp:
+        mapping = pickle.load(fp)
+
+    sns.heatmap(
+        mapping,
+        xticklabels=False,
+        yticklabels=False,
+        cmap="RdBu_r",
+        ax=ax[1, i],
+        center=0,
+        cbar=False,
+        cbar_kws={"shrink": 0.7},
+    )
+    ax[1, i].set_aspect("equal")
+
+    if i == 0:
+        ax[1, i].set_ylabel("KDF - RF Noise Error")
+
+# KDN plotting
+for i, f in enumerate(func_names):
+    fname = "data/" + f + "_mapping_kdn.pkl"
+    with open(fname, "rb") as fp:
+        mapping = pickle.load(fp)
+
+    sns.heatmap(
+        mapping,
+        xticklabels=False,
+        yticklabels=False,
+        cmap="RdBu_r",
+        ax=ax[2, i],
+        center=0,
+        cbar=False,
+    )
+    ax[2, i].set_aspect("equal")
+
+    if i == 0:
+        ax[2, i].set_ylabel("KDN vs. DN Noise Error")
+
 plt.tight_layout()
-plt.savefig("plots/label_noise_kdf.pdf")
-
-# Create KDN figure
-with open("data/xor_heatmap_kdn.pkl", "rb") as f:
-    mapping = pickle.load(f)
-
-# Setup some plotting parameters
-sns.set_context("talk")
-fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-p0s = np.arange(0, 0.5, 0.05)
-p1s = np.arange(0, 0.5, 0.05)
-p0_labels = ["{:.2f}".format(p0) for p0 in p0s]
-p1_labels = ["{:.2f}".format(p1) for p1 in p1s]
-
-# Make every other label invisible
-for j, p1 in enumerate(p1s):
-    if j % 2 == 0:
-        p1_labels[j] = ""
-        p0_labels[j] = ""
-
-sns.heatmap(
-    mapping,
-    xticklabels=p1_labels,
-    yticklabels=p0_labels,
-    cmap="RdBu_r",
-    ax=ax,
-    cbar=True,
-    cbar_kws={"shrink": 0.7},
-    vmin=-0.2,
-    vmax=0.2,
-)
-ax.set_xlabel("p1")
-ax.set_ylabel("p0")
-ax.set_aspect("equal", "box")
-plt.title("Gaussian XOR Label Noise: KDN - DN Errors")
-plt.tight_layout()
-plt.savefig("plots/label_noise_kdn.pdf")
+plt.savefig("plots/label_noise_heatmaps.pdf")
